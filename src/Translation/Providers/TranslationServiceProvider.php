86cd822f92c6aaa49eb366fc7a7bd5fad2b2b80b
Rafał Krawiec <rafal.krawiec16@gmail.com>
2024-10-11 13:09:53 +0200

    Add validation translations for polish locale.

--- src/Translation/Providers/TranslationServiceProvider.php
+++ src/Translation/Providers/TranslationServiceProvider.php
@@ -11,10 +11,10 @@ class TranslationServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../../../config/translations.php' => config_path('translations.php'),
-            ], 'config');
+            $this->publishes([__DIR__ . '/../../../config/translations.php' => config_path('translations.php')], 'config');
         }
+
+        $this->loadJsonTranslationsFrom(__DIR__ . '/../../../lang/validation');
     }
 
 
