2a640d3a43dc5a98f49b54421f3ce1347a2e730d
Rafał Krawiec <rafal.krawiec16@gmail.com>
2025-01-10 12:04:33 +0100

    Implement custom serialize date in model.

--- src/Database/Eloquent/Model.php
+++ src/Database/Eloquent/Model.php
@@ -5,8 +5,14 @@
 namespace OtherSoftware\Database\Eloquent;
 
 
+use Carbon\CarbonImmutable;
+use DateTimeImmutable;
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
+use IntlDateFormatter;
+use IntlDatePatternGenerator;
 use Override;
 
 
@@ -67,5 +73,23 @@ abstract class Model extends EloquentModel
     public function toArray()
     {
         return array_camel_keys(parent::toArray());
+    }
+
+
+    #[Override]
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        $instance = $date instanceof DateTimeImmutable ? CarbonImmutable::instance($date) : Carbon::instance($date);
+        $locale = config('app.locale');
+
+        if ($instance->isStartOfDay()) {
+            $pattern = (new IntlDatePatternGenerator($locale))->getBestPattern('dd/MM/yyyy');
+        } else {
+            $pattern = (new IntlDatePatternGenerator($locale))->getBestPattern('dd/MM/yyyy HH:mm');
+        }
+
+        $formatter = new IntlDateFormatter(locale: $locale, pattern: $pattern);
+
+        return $formatter->format($instance);
     }
 }
