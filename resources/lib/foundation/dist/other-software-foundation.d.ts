687de7d38797957a79213fa44969a3a22d3a8dab
Rafał Krawiec <rafal.krawiec16@gmail.com>
2025-06-25 07:21:28 +0200

    Build.

--- resources/lib/foundation/dist/other-software-foundation.d.ts
+++ resources/lib/foundation/dist/other-software-foundation.d.ts
@@ -633,3 +633,43 @@ export declare type ViewResolver = (name: string) => StackedViewComponent;
 export declare function wrap<T>(item: T): T[] & any[];
 
 export { }
+
+declare global {
+  declare const APP_NAME: string;
+  declare const APP_ENV: string;
+  declare const APP_DEBUG: boolean;
+  declare const APP_TIMEZONE: string;
+  declare const APP_URL: string;
+  declare const APP_LOCALE: string;
+  declare const APP_AVAILABLE_LOCALES: string[];
+  declare const APP_FALLBACK_LOCALE: string;
+  declare const APP_COUNTRY: string;
+  declare const APP_COUNTRIES_IMPLICITLY_ADDRESSES: string[];
+  declare const APP_CURRENCY: string;
+  declare const APP_ROUTES: Record<string, any>;
+  declare const APP_TRANSLATIONS: Record<string, any>;
+}
+
+declare global {
+  type Nullable<T> = T | null | undefined;
+  type PhoneNumber = { national: string, country: string, rfc: string };
+}
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    RouterFrame: typeof RouterFrameComponent,
+    RouterNested: typeof RouterNestedComponent,
+    RouterView: typeof RouterViewComponent,
+    RouterLink: typeof RouterLinkComponent,
+    FormController: typeof FormControllerComponent,
+    ToastController: typeof ToastControllerComponent,
+    PasswordConfirmationController: typeof PasswordConfirmationControllerComponent,
+    Toast: typeof ToastComponent,
+  }
+
+  export interface ComponentCustomProperties {
+    $t: typeof trans,
+    $tc: typeof transChoice,
+    $route: typeof route,
+  }
+}
