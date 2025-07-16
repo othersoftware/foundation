74b2678e732fcf72c1d73cf159073ba232c288c8
Rafał Krawiec <rafal.krawiec16@gmail.com>
2025-07-16 17:46:24 +0200

    FIX

--- resources/lib/vite-plugin-vue-autoloader/src/Services/ViewRouterTransformer.ts
+++ resources/lib/vite-plugin-vue-autoloader/src/Services/ViewRouterTransformer.ts
@@ -3,14 +3,20 @@ import type { ResolvedConfig } from 'vite';
 import { collect } from './Collector.ts';
 import type { Options } from '../Types/Options.ts';
 
-let views: Set<string>;
+let viewsMap: Map<string, Set<string>> = new Map;
 
 function collectViewsSet(config: ResolvedConfig, options: Options) {
   let collection = collect(config, options.views, options.namespace);
+  let namespace = options.namespace || 'default';
+  let views = new Set() as Set<string>;
 
-  views = new Set();
+  viewsMap.set(namespace, views);
 
   collection.components.forEach(component => {
+    views.add(component.path);
+  });
+
+  collection.vendors.forEach(component => {
     views.add(component.path);
   });
 }
@@ -20,14 +26,14 @@ export function resetViewsSet(config: ResolvedConfig, options: Options) {
 }
 
 export function transformViewComponent(config: ResolvedConfig, options: Options, code: string, id: string) {
-  if (!views) {
+  if (!viewsMap.has(options.namespace || 'default')) {
     collectViewsSet(config, options);
   }
 
   const resolvedId = path.normalize(id);
 
   // Check if the file is one of views components and is a Vue component
-  if (!id.endsWith('.vue') || !views.has(resolvedId)) {
+  if (!id.endsWith('.vue') || !viewsMap.get(options.namespace || 'default')!.has(resolvedId)) {
     return { code, map: null };
   }
 
