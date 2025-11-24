cdba1d22c02063176682a668e8ea9054769c6a84
Rafał Krawiec <rafal.krawiec16@gmail.com>
2025-11-24 15:30:19 +0100

    WIP

--- helpers/errors.php
+++ helpers/errors.php
@@ -23,17 +23,40 @@ if (! function_exists('report_with_result')) {
      * @param TResult $result
      *
      * @return TResult
-     * @noinspection PhpDocMissingThrowsInspection
      */
     function report_with_result(Throwable|string $exception, mixed $result = false): mixed
     {
         if (app()->isLocal()) {
-            /** @noinspection PhpUnhandledExceptionInspection */
             throw new LocalDebuggingException(is_string($exception) ? new Exception($exception) : $exception);
         }
 
         report($exception);
 
         return $result;
+    }
+}
+
+if (! function_exists('reportWith')) {
+    /**
+     * Reports given exception and returns a given result, if in production.
+     * By default, a `false` value is returned.
+     *
+     * @template TException of (Throwable|string)
+     * @template TResult of mixed
+     *
+     * @param TException $exception
+     * @param TResult|Closure(TException $exception):TResult $result
+     *
+     * @return TResult
+     */
+    function reportWith(Throwable|string $exception, mixed $result = false): mixed
+    {
+        if (app()->isLocal()) {
+            throw new LocalDebuggingException(is_string($exception) ? new Exception($exception) : $exception);
+        }
+
+        report($exception);
+
+        return value($result, $exception);
     }
 }
